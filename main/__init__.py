ece7cce25cc0567c4c13585341a30388c1570f84
Moritz Tiedje <mtiedje@andrena.de>
2017-01-29 18:34:40 +0100

    Cleanup

--- main/__init__.py
+++ main/__init__.py
@@ -7,24 +7,12 @@ display_height = 600
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 clock = pygame.time.Clock()
 gameOver = False
-border = pygame.image.load('../artwork/border.png')
-
-
-
-
-
-def field(x, y):
-    gameDisplay.blit(border, (x, y))
-
-x =  (display_width * 0.45)
-y = (display_height * 0.8)
-x_change = 0
-car_speed = 0
+border = pygame.image.load('../artwork/images/border.png')
 
 while not gameOver:
     for event in pygame.event.get():
@@ -32,15 +20,13 @@ while not gameOver:
             gameOver = True
 
     gameDisplay.fill(white)
-    for x in range(0,8):
-        for y in range(0,8):
-            gameDisplay.blit(border, (- 100 + x*208,  - 100 + y*104))
-            gameDisplay.blit(border, (- 100 + x*208 + 104, -100 + 52 + y*104))
+    for x in range(0, 8):
+        for y in range(0, 8):
+            gameDisplay.blit(border, (- 100 + x * 208, - 100 + y * 104))
+            gameDisplay.blit(border, (- 100 + x * 208 + 104, -100 + 52 + y * 104))
 
     pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
 quit()
-
-
