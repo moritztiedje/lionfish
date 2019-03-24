aa7efb007616306a89169fbb97d034e0c7085f09
Moritz Tiedje <moritztiedje@gmail.com>
2019-03-24 21:01:47 +0100

    Automatic refactoring

--- src/main/GUI/BaseComponents/button.py
+++ src/main/GUI/BaseComponents/button.py
@@ -1,5 +1,6 @@
 from src.main.Util.point import Point
 
+
 class Button:
     def __init__(self, bottom_left, top_right, image, action):
         """
@@ -14,7 +15,7 @@ class Button:
 
     def handle_click(self, mouse_position):
         if self.__bottom_left.get_x() <= mouse_position[0] <= self.__top_right.get_x() and \
-                self.__bottom_left.get_y() <= mouse_position[1] <= self.__top_right.get_y():
+                                self.__bottom_left.get_y() <= mouse_position[1] <= self.__top_right.get_y():
             self.__action()
 
     def display(self, game_window):
