1741dba6b8e26e911c04dfb4f03b075467128043
Moritz Tiedje <moritztiedje@gmail.com>
2019-03-25 18:19:42 +0100

    Refactoring. Menu Panel Buttons created in Menu Panel

--- src/main/GUI/View/panels/menuPanel.py
+++ src/main/GUI/View/panels/menuPanel.py
@@ -1,13 +1,38 @@
+import pygame
+
+from src.main.GUI.BaseComponents.button import Button
+from src.main.GUI.View.image import Image
 from src.main.GUI.View.imageVaults.menuImageVault import MenuImageVault
 from src.main.GUI.View.panels.panel import Panel
+from src.main.Util.point import Point
 
 
 class MenuPanel(Panel):
-    def __init__(self, game_window):
+    def __init__(self, game_window, zoom_in, zoom_out):
         """
         :type game_window: src.main.GUI.View.gameWindow.GameWindow
         """
         super().__init__(game_window, 1)
+        main_menu_button = Button(Point(game_window.get_width() - 40, game_window.get_height() - 40),
+                                  Point(game_window.get_width() - 10, game_window.get_height() - 10),
+                                  Image(0, 0, '../../artwork/images/menu/gear.png'),
+                                  self.__quit_game)
+        zoom_in_button = Button(Point(game_window.get_width() - 40, game_window.get_height() - 80),
+                                Point(game_window.get_width() - 10, game_window.get_height() - 50),
+                                Image(0, 0, '../../artwork/images/menu/magnify.png'),
+                                zoom_in)
+        zoom_out_button = Button(Point(game_window.get_width() - 40, game_window.get_height() - 120),
+                                 Point(game_window.get_width() - 10, game_window.get_height() - 90),
+                                 Image(0, 0, '../../artwork/images/menu/reduce.png'),
+                                 zoom_out)
+        self.register_button(main_menu_button)
+        self.register_button(zoom_in_button)
+        self.register_button(zoom_out_button)
+
+    @staticmethod
+    def __quit_game():
+        pygame.quit()
+        quit()
 
     def _handle_mouse_event(self, mouse_event):
         pass
--- src/main/GUI/View/panelsManager.py
+++ src/main/GUI/View/panelsManager.py
@@ -19,7 +19,7 @@ class PanelsManager:
         self.__panels = {
             Panels.AreaMap: self.__build_area_map_view(game_window),
             Panels.WorldMap: self.__build_world_map_view(game_window),
-            Panels.MainMenuBar: self.__build_main_menu_view(game_window),
+            Panels.MainMenuBar: MenuPanel(game_window, self.__camera_zoom_in, self.__camera_zoom_out),
             Panels.TextAdventureBox: self.__build_text_adventure_panel(game_window)
         }
 
@@ -31,25 +31,6 @@ class PanelsManager:
         area_map_view = AreaMapPanel(game_window)
         area_map_view.register_button(world_map_button)
         return area_map_view
-
-    def __build_main_menu_view(self, game_window):
-        main_menu_button = Button(Point(game_window.get_width() - 40, game_window.get_height() - 40),
-                                  Point(game_window.get_width() - 10, game_window.get_height() - 10),
-                                  Image(0, 0, '../../artwork/images/menu/gear.png'),
-                                  self.__quit_game)
-        zoom_in_button = Button(Point(game_window.get_width() - 40, game_window.get_height() - 80),
-                                Point(game_window.get_width() - 10, game_window.get_height() - 50),
-                                Image(0, 0, '../../artwork/images/menu/magnify.png'),
-                                self.__camera_zoom_in)
-        zoom_out_button = Button(Point(game_window.get_width() - 40, game_window.get_height() - 120),
-                                 Point(game_window.get_width() - 10, game_window.get_height() - 90),
-                                 Image(0, 0, '../../artwork/images/menu/reduce.png'),
-                                 self.__camera_zoom_out)
-        main_menu_view = MenuPanel(game_window)
-        main_menu_view.register_button(main_menu_button)
-        main_menu_view.register_button(zoom_in_button)
-        main_menu_view.register_button(zoom_out_button)
-        return main_menu_view
 
     @staticmethod
     def __build_world_map_view(game_window):
@@ -67,14 +48,16 @@ class PanelsManager:
         return GameStateChangeEvent(GameStateChangeEventTypes.GoToWorldMap, None)
 
     def __camera_zoom_in(self):
-        for view in self.__panels:
-            if view.is_active():
-                view.zoom_in()
+        if self.__panels[Panels.AreaMap].is_active():
+            self.__panels[Panels.AreaMap].zoom_in()
+        if self.__panels[Panels.WorldMap].is_active():
+            self.__panels[Panels.WorldMap].zoom_in()
 
     def __camera_zoom_out(self):
-        for view in self.__panels:
-            if view.is_active():
-                view.zoom_out()
+        if self.__panels[Panels.AreaMap].is_active():
+            self.__panels[Panels.AreaMap].zoom_out()
+        if self.__panels[Panels.WorldMap].is_active():
+            self.__panels[Panels.WorldMap].zoom_out()
 
     def draw(self, game_state):
         """
