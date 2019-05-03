c1ba3d1ccc474f4ca74db1aceea56576297ea32f
Moritz Tiedje <moritztiedje@gmail.com>
2019-05-03 23:01:13 +0200

    Refactoring

--- src/main/GUI/View/panels/gameOverPanel.py
+++ src/main/GUI/View/panels/gameOverPanel.py
@@ -3,12 +3,12 @@ import pygame
 from src.main.GUI.BaseComponents.geometry import Point
 from src.main.GUI.View.imageVaults.gameOverImageVault import GameOverImageVault
 from src.main.GUI.View.panels.panel import Panel
-from src.main.GUI.View.panels.textAdventurePanel import RenderedText
 from src.main.constants import GameOverImageEnum
 
 HEIGHT_OF_LINE = 60
 FONT = 'Arial Black'
 TEXT_COLOR = 'darkblue'
+SIDE_MARGIN = 100
 
 
 class GameOverPanel(Panel):
@@ -35,64 +35,95 @@ class GameOverPanel(Panel):
         background.scale_to_width(self._game_window.get_width())
         self._game_window.draw(background.sprite, Point(0, self._game_window.get_height()))
 
-        available_text_width = self._game_window.get_width() - 200
-        text = RenderedLine(game_state.get_game_over_text(), available_text_width)
+        available_text_width = self._game_window.get_width() - SIDE_MARGIN * 2
+        text = RenderedText(game_state.get_game_over_text(), available_text_width)
         draw_coordinate_of_word = Point(
-                (self._game_window.get_width() - text.get_width()) / 2,
+                SIDE_MARGIN,
                 (self._game_window.get_height() + text.get_height()) / 2)
         text.draw(draw_coordinate_of_word, self._game_window.draw)
 
 
-class RenderedLine:
+class RenderedText:
     def __init__(self, text, available_width):
         """
         :type text: str
         :type available_width: int
         """
-        self.__words = []
+        self.__lines = []
         self.__height = 0
-        self.__width = available_width
 
         font = pygame.font.SysFont(FONT, HEIGHT_OF_LINE)
         text_color = pygame.Color(TEXT_COLOR)
 
+        current_line_number = 0
+        current_line = RenderedLine(current_line_number)
         words = text.split(' ')
-        draw_coordinate_of_word = Point(0, 0)
-        draw_coordinate_of_next_word = Point(0, 0)
+        for word_str in words:
+            word = font.render(word_str, 0, text_color)
+            if current_line.word_makes_line_too_long(word, available_width):
+                current_line.center(available_width)
+                self.__lines.append(current_line)
+                current_line_number += 1
+                current_line = RenderedLine(current_line_number)
+            current_line.add(word)
+
+        self.__height = HEIGHT_OF_LINE * (current_line_number + 1)
+        current_line.center(available_width)
+        self.__lines.append(current_line)
+
+    def draw(self, coordinate, _draw_method):
+        for line in self.__lines:
+            line.draw(coordinate, _draw_method)
+
+    def get_height(self):
+        return self.__height
+
+
+class RenderedLine:
+    def __init__(self, line_number):
+        self.__words = []
+        self.__current_word_x_position = 0
+        self.__y_coordinate = - line_number * HEIGHT_OF_LINE
+
+    def add(self, word_surface):
+        word_draw_coordinate = Point(self.__current_word_x_position, self.__y_coordinate)
+        word = RenderedWord(word_surface, word_draw_coordinate)
+        self.__words.append(word)
+
+        font = pygame.font.SysFont(FONT, HEIGHT_OF_LINE)
         space_width = font.render(" ", 0, pygame.Color('black')).get_width()
-        for word in words:
-            rendered_word = font.render(word, 0, text_color)
-            word_width = rendered_word.get_width()
-            if draw_coordinate_of_word.get_x() + word_width >= available_width:
-                draw_coordinate_of_word = Point(0, draw_coordinate_of_word.get_y() - HEIGHT_OF_LINE)
-            draw_coordinate_of_next_word = draw_coordinate_of_word + Point(word_width + space_width, 0)
-            self.__words.append(RenderedWord(rendered_word, draw_coordinate_of_word))
-            self.__width = max(draw_coordinate_of_word.get_x(), self.__width)
-
-            draw_coordinate_of_word = draw_coordinate_of_next_word
-
-        self.__height = HEIGHT_OF_LINE - draw_coordinate_of_next_word.get_y()
+        self.__current_word_x_position += word_surface.get_width() + space_width
 
     def draw(self, coordinate, _draw_method):
         for word in self.__words:
             word.draw(coordinate, _draw_method)
 
-    def get_height(self):
-        return self.__height
+    def center(self, available_width):
+        last_word = self.__words[len(self.__words) - 1]
+        line_width = last_word.get_right_side()
+        for word in self.__words:
+            word.shift_right((available_width - line_width) / 2)
 
-    def get_width(self):
-        return self.__width
+    def word_makes_line_too_long(self, rendered_word, available_width):
+        word_width = rendered_word.get_width()
+        return self.__current_word_x_position + word_width >= available_width
 
 
 class RenderedWord:
     def __init__(self, word, draw_coordinate):
-        """
-        :type word: pygame.ftfont.Font
-        :type draw_coordinate: src.main.GUI.BaseComponents.geometry.Point
-        """
         self.__rendered_word = word
         self.__draw_coordinate = draw_coordinate
 
-    def draw(self, coordinate, _draw_relative_to_camera):
+    def draw(self, coordinate, draw_method):
         draw_coordinate = coordinate + self.__draw_coordinate
-        _draw_relative_to_camera(self.__rendered_word, draw_coordinate)
+        print(draw_coordinate.get_x())
+        draw_method(self.__rendered_word, draw_coordinate)
+
+    def shift_right(self, shift_by):
+        self.__draw_coordinate += Point(shift_by, 0)
+
+    def get_right_side(self):
+        return self.__draw_coordinate.get_x() + self.__rendered_word.get_width()
+
+    def get_width(self):
+        return self.__rendered_word.get_width()
